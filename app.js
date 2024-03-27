2df82ceed1b2970ef2fa46a7dd07f4ca3f0c05aa
WolfDab <BuildingWolf100@outlook.com>
2024-03-26 20:21:48 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const colors = require('ansi-colors')
+
+console.log(colors.red("red text"))
+console.log(colors.green("green text"))
+console.log(colors.blue("blue text"))
+
+console.log(colors.bgRed("red background"))
+
+console.log(colors.black.bgWhite("black text, white background"))
